d5452e868bf86e2d6f338e98bfb5687421d7e120
ToMyu <ToMyu.uan@foxmail.com>
2016-10-31 20:18:03 +0800

    1st

---
+++ Git/src/bean/Hello.java
@@ -0,0 +1,13 @@
+package bean;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println("line1");
+		System.out.println("line2");
+		System.out.println("line3");
+		System.out.println("line4");
+		System.out.println("line5");
+	}
+	
+}
